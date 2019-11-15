7a36c87c1de25eb1e17bd7b3642a6634a5949bcc
Jerry Shi <jerryshi25@gmail.com>
2019-11-15 20:40:05 +0000

    finished all tasks except for monthly payment :/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,45 @@ class ApplicationController < ActionController::Base
 
     render({ :plain => output_message})
   end
+
+  def query_square
+    the_num = params.fetch("input_number").to_f
+    the_square = the_num ** 2
+
+    output_message = "{\"number\": #{the_num}, \"square\": #{the_square}}"
+
+    render({ :plain => output_message})
+  end
+
+  def query_sqrt
+    the_num = params.fetch("input_number").to_f
+    the_sqrt = Math.sqrt(the_num)
+
+    output_message = "{\"number\": #{the_num}, \"square root\": #{the_sqrt}}"
+
+    render({ :plain => output_message})
+  end
+
+  def query_random
+    min = params.fetch("input_min").to_f
+    max = params.fetch("input_max").to_f
+    random = rand((min..max))
+
+    output_message = "{\"min\": #{min}, \"max\": #{max}, \"random\": #{random}}"
+
+    render({ :plain => output_message})
+  end
+
+  def payment
+    apr = params.fetch("input_apr").to_f
+    years = params.fetch("input_years").to_f
+    pv = params.fetch("input_pv").to_f
+    purchase_price = (apr * pv) / (1 - (1 + apr)**-years)
+    monthly_payment = purchase_price / (years * 12)
+
+    output_message = "{\"purchase_price\": #{purchase_price}, \"apr\": #{apr}, \"years\": #{years}, \"monthly_payment\": #{monthly_payment}}"
+
+    render({ :plain => output_message})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
 
   match("/flexible/random/:min/:max", { :controller => "application", :action => "random", :via => "get" })
 
+  match("/square/results", { :controller => "application", :action => "query_square", :via => "get" })
+
+  match("/square_root/results", { :controller => "application", :action => "query_sqrt", :via => "get" })
+
+  match("/random/results", { :controller => "application", :action => "query_random", :via => "get" })
+
+  match("/payment/results", { :controller => "application", :action => "payment", :via => "get" })
+
 end
